f78aca3b00c20af8e16348ca4b8a0a45859bb186
bunty <bunty@Buntys-MacBook-Air.local>
2024-02-11 13:13:56 +0530

    update

--- main.js
+++ main.js
@@ -6,31 +6,6 @@ hamburger.addEventListener("click", ()=>{
 })
 
 
-
-// var swiper = new Swiper(".mySwiper", {
-//   slidesPerView: 3,
-//   spaceBetween: 30,
-//   autoplay:{
-//     delay: 2300
-//   },
-//   breakpoints:{
-//     1200:{
-//         slidesPerView:3,
-//     }, 
-//     800:{
-//         slidesPerView:2,
-//     },
-//     575:{
-//         slidesPerView:1,
-//     }
-//   },
-//   pagination: {
-//     el: ".swiper-pagination",
-//     clickable: true,
-//   },
-// });
-
-
 $('.swiper-wrapper').slick({
     centerMode: true,
     centerPadding: '0px',
